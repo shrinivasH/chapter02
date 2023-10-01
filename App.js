3de768ea7baae8107425790a66d133e82dc7650c
User <user@Users-MacBook-Pro-136.local>
2023-10-02 00:05:23 +0530

    Episode 02

---
+++ App.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const parent = React.createElement("h1", {}, "Namaste React! ");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
